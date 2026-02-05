f90be2d2398c1e472ecf9cec00abf87612342260
Matthew Chan <matthewhohinchan@gmail.com>
2026-02-04 16:17:58 -0800

    Start Go database project

---
+++ 0101/kv.go
@@ -0,0 +1,37 @@
+package db0101
+
+import "bytes"
+
+type KV struct {
+	mem map[string][]byte
+}
+
+func (kv *KV) Open() error {
+	kv.mem = map[string][]byte{} // empty
+	return nil
+}
+
+func (kv *KV) Close() error { return nil }
+
+func (kv *KV) Get(key []byte) (val []byte, ok bool, err error) {
+	val, ok = kv.mem[string(key)]
+	return
+}
+
+func (kv *KV) Set(key []byte, val []byte) (updated bool, err error) {
+	prev, exist := kv.mem[string(key)]
+	kv.mem[string(key)] = val
+	updated = !exist || !bytes.Equal(prev, kv.mem[string(key)])
+	return
+}
+
+func (kv *KV) Del(key []byte) (deleted bool, err error) {
+	_, deleted = kv.mem[string(key)]
+	if deleted {
+		delete(kv.mem, string(key))
+	}
+	return
+
+}
+
+// QzBQWVJJOUhU https://trialofcode.org/
